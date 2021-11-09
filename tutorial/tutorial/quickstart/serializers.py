bb38ed356428e94721b484b9204327412ee654c4
heyoni <hhheyoni@gmail.com>
2021-11-09 13:49:59 +0900

    Apply .gitinore

--- tutorial/tutorial/quickstart/serializers.py
+++ tutorial/tutorial/quickstart/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from snippets.models import Snippet
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
+from django.contrib import admin
 from tutorial.quickstart import views
+import debug_toolbar
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -9,6 +11,8 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include('snippets.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('instagram.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
