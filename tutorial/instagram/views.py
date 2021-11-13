caae880e5c806beaa26007e57d25dae43f228e80
heyoni <hhheyoni@gmail.com>
2021-11-13 23:58:51 +0900

    pagination

--- tutorial/instagram/views.py
+++ tutorial/instagram/views.py
@@ -1,8 +1,9 @@
+from django.db import models
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import get_object_or_404, render, redirect
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 
 def post_list(request):
@@ -31,4 +32,22 @@ def post_list(request):
 #     })
 
 # 위 내용과 아래 내용은 같다!
-post_detail = DetailView.as_view(model=Post)
+# post_detail = DetailView.as_view(
+#   model=Post,
+#   queryset=Post.objects.filter(is_public=True) # 공개된 포스팅만 보여짐
+# )
+
+# 로그인한 사용자는 전부 다 볼 수있고 로그인 안하면 볼 수없도록 만들고 싶을 때는
+# 상속을 통해 클래스를 재정의 해야한다.
+class PostDetailView(DetailView):
+    model = Post
+    # queryset = Post.objects.filter(is_public=True)
+    
+    def get_queryset(self):
+        # 재정의할땐 보통 super로 호출함
+        qs = super().get_queryset() # 부모의 function을 호출하여 부모가 만들어진 쿼리셋을 가져오고
+        if not self.request.user.is_authenticated: # get_queryset은 원래 인자를 받지 않으나 self.request에서 받을 수 있음 -> 여기서는 로그인한 유저의 인스턴스를 가져옴 -> 이 문장은 '로그인이 안되어 있으면 공개된 것만 볼 수 있다'라는 뜻
+            qs = qs.filter(is_public=True)
+        return qs
+post_detail = PostDetailView.as_view(model=Post)
+post_list = ListView.as_view(model=Post, paginate_by=10)
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,15 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 from django.contrib import admin
-from tutorial.quickstart import views
+# from tutorial.quickstart import views
 import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -20,7 +20,7 @@ urlpatterns = [
     path('instagram/', include('instagram.urls')),
     path('accounts/',include('accounts.urls')),
     path('', TemplateView.as_view(template_name='root.html'), name='root'),
-    path('', RedirectView.as_view(template_name='root.html'), name='root'),
+    # path('', RedirectView.as_view(template_name='root.html'), name='root'),
 
 ]
 
