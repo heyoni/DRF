6aabd81966328623974525cd4f4200aed3b774b8
heyoni <hhheyoni@gmail.com>
2021-11-23 17:57:10 +0900

    new project

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,6 +1,16 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from . import views
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import LoginForm
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login')
-]
+    path('login/', LoginView.as_view(
+        form_class = LoginForm,
+        template_name='accounts/login_form.html'
+    ), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/',views.profile, name='profile'),
+    path('profile/update/', views.profile_update, name='profile_update'),
+    path('signup/', views.signup,'name=signup'),
+] 
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import get_user_model, login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, CreateView
 
-# Create your views here.
+# @login_required
+# def profile(request):
+#     return render(request, 'accounts/profile.html')
+from accounts.forms import ProfileForm
+from accounts.models import Profile
+
+
+User = get_user_model()
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
+
+
+profile = ProfileView.as_view()
+
+
+@login_required
+def profile_update(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile_form.html', {
+        'form': form,
+    })
+
+
+# 회원가입하면 로그인페이지로 넘어감
+User = get_user_model()
+signup = CreateView.as_view(
+    model=User,
+    form_class = UserCreationForm,
+    success_url = settings.LOGIN_URL,
+    template_name = 'accounts/signup_form.html',
+)
+
+# # 회원가입 하자마자 로그인 해줌
+# class SignupView(CreateView):
+#     model = User
+#     form_class = UserCreationForm
+#     success_url = settings.LOGIN_REDIRECT_URL
+#     template_name = 'accounts/signup_form.html'
+
+#     def form_valid(self, form):
+#         response = super().form_valid(form)
+#         user = self.object
+#         auth_login(self.request, user)
+#         return response
+# 
+# signup = SignupView.as_view()
