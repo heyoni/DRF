7e1140c36ce6c3f7fbbcb2e8746ceb2dff5b3f12
heyoni <hhheyoni@gmail.com>
2021-11-24 18:00:42 +0900

    style

--- instagram/tutorial/urls.py
+++ instagram/tutorial/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import include, path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('', TemplateView.as_view(template_name='root.html'), name='root'),
 ]
 
 if settings.DEBUG:
